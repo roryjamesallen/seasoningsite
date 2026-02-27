af9376019f64b88c86f5d6aec8bfcc49e3c30291
roryjamesallen <rory.james.allen1@gmail.com>
2026-02-27 16:34:25 +0000

    proper font with goo

--- index.php
+++ index.php
@@ -1,22 +1,26 @@
 <html>
     <head>
-	<link rel="stylesheet" href="style.css?v=10">
+	<link rel="stylesheet" href="style.css?v=11">
     </head>
     <body>
-	<h1><span>S</span><span>e</span><span>a</span><span>s</span><span>o</span><span>n</span><span>i</span><span>n</span><span>g</span></h1>
-	<div id="toolbar">
+	<h1 class="goo moving-children"><span>S</span><span>e</span><span>a</span><span>s</span><span>o</span><span>n</span><span>i</span><span>n</span><span>g</span></h1>
+	<div id="toolbar" class="moving-children">
 	    <a id="insta-link" href="https://www.instagram.com/seas0ning_/"><img src="images/icons/instagram.png"></a>
 	    <a id="ra-link" href="https://ra.co/promoters/119677"><img src="images/icons/resident-advisor.png"></a>
 	</div>
     </body>
 </html>
 <script>
- const h1 = document.getElementsByTagName('h1')[0];
- const h1_letter_offset_rem = 0.5;
+ const movement_px = 10;
 
  function moveTitleLetters(){
-     for (let i=0; i<h1.children.length; ++i){
-	 h1.children[i].style.transform = 'translate('+(((Math.random() * 2) - 1) * h1_letter_offset_rem) + 'rem, '+(((Math.random() * 2) - 1) * h1_letter_offset_rem) + 'rem)';
+     for (let j=0; j<document.getElementsByClassName('moving-children').length; ++j){
+	 const parent = document.getElementsByClassName('moving-children')[j];
+	 for (let i=0; i<parent.children.length; ++i){
+	     const x_px = Math.floor((((Math.random() * 2) - 1) * movement_px));
+	     const y_px = Math.floor((((Math.random() * 2) - 1) * movement_px));
+	     parent.children[i].style.transform = 'translate3d('+x_px+'px, '+y_px+ 'px, 0)';
+	 }
      }
  }
 

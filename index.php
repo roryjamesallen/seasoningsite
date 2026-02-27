4f0e18f71106987f9a1004ce5044505e5a8b4953
roryjamesallen <rory.james.allen1@gmail.com>
2026-02-27 13:53:23 +0000

    better homepage

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	<link rel="stylesheet" href="style.css">
     </head>
     <body>
-	<h1 id="title-container">
+	<h1 id="title-container" class="morph-container">
 	    <span>S</span>
 	    <span>e</span>
 	    <span>a</span>
@@ -14,49 +14,63 @@
 	    <span h-offset="-0.75">n</span>
 	    <span>g</span>
 	</h1>
+	<div id="toolbar">
+	    <a id="insta-link" href="https://www.instagram.com/seas0ning_/"><img src="images/icons/instagram.png" width="50px"></a>
+	    <a id="ra-link" href="https://ra.co/promoters/119677"><img src="images/icons/resident-advisor.png" width="100px"></a>
+	</div>
     </body>
 </html>
 <script>
- // Setup Constants
- const title_size_rem = 12;
- 
  // Element Constants
  const title_letters = document.getElementById('title-container');
+ const insta_letters = document.getElementById('insta-link');
+ const ra_letters = document.getElementById('ra-link'); 
 
+ // Helpers
+ function pxToRem(px){
+     return px / parseFloat(getComputedStyle(document.documentElement).fontSize);
+ }
+ 
  // Dynamic Elements
- function updateMovingLetters(element){
-     const max_letter_offset_rem = 0.5; // +- in both directions
+ function updateMovingLetters(element, max_letter_offset_rem){
      for (let i=0; i<element.children.length; ++i){
 	 let horizontal_offset_rem = (Math.random()*2) * max_letter_offset_rem;
 	 const vertical_offset_rem =  (Math.random()*2) * max_letter_offset_rem;
-	 element.children[i].style.transform = 'translate('+horizontal_offset_rem+'rem, '+vertical_offset_rem+'rem)';
+	 element.children[i].style.transform = 'translate('+horizontal_offset_rem+'rem, '+vertical_offset_rem+'rem) translate3d(0,0,0)';
      }
  }
 
  // Initialisers
- function initialiseTitleLetters(){
-     const letter_width_rem = 4.5;
-     const centre_offset_rem = (title_letters.children.length * -letter_width_rem) / 2;
-     for (let i=0; i<title_letters.children.length; ++i){
-	 let horizontal_offset_rem = centre_offset_rem + (i * letter_width_rem);
-	 if (title_letters.children[i].hasAttribute('h-offset')){
-	     horizontal_offset_rem = horizontal_offset_rem + parseFloat(title_letters.children[i].getAttribute('h-offset'));
+ function initialiseMovingLetters(element, centred=false){
+     letter_width_rem = pxToRem(parseFloat(getComputedStyle(element).fontSize)) / 2.75;
+     for (let i=0; i<element.children.length; ++i){
+	 let horizontal_offset_rem = i * letter_width_rem;
+	 if (element.children[i].hasAttribute('h-offset')){
+	     horizontal_offset_rem = horizontal_offset_rem + parseFloat(element.children[i].getAttribute('h-offset'));
 	 }
-	 title_letters.children[i].style.left = horizontal_offset_rem+'rem';
+	 if (centred){
+	     horizontal_offset_rem += (element.children.length * -letter_width_rem) / 2;
+	     element.children[i].style.left = 'calc(50vw + '+horizontal_offset_rem+'rem)';
+	 } else {
+	     element.children[i].style.left = horizontal_offset_rem+'rem';
+	 }
      }
  }
- function initialiseMovingLetters(){
-     initialiseTitleLetters();
-     
-     title_letters.style.transition = 'transform 1s ease-out, opacity 1s ease-out';
-     title_letters.style.opacity = '1';
-     //updateMovingLetters(title_letters);
-     setTimeout(function(){ title_letters.style.transition = 'transform 5s ease-in-out'; }, 1000);
-     title_letters.style.fontSize = title_size_rem + 'rem';
-     setInterval(function(){ updateMovingLetters(title_letters); }, 2500);
+ function initialiseSelectMovingLetters(elements){
+     for (let i=0; i<elements.length; ++i){
+	 const element = elements[i][0];
+	 initialiseMovingLetters(element,elements[i][3]);
+	 element.style.transition = 'transform 1s ease-out, opacity 1s ease-out';
+	 element.style.opacity = '1';
+	 updateMovingLetters(element, elements[i][1]);
+	 if (elements[i][2] == true){ //animated
+	     setTimeout(function(){ element.style.transition = 'transform 5s ease-in-out'; }, 1000);
+	     setInterval(function(){ updateMovingLetters(element, elements[i][1]); }, 2500);
+	 }
+     }
  }
 
  window.onload = (event) => {
-     initialiseMovingLetters();
+     initialiseSelectMovingLetters([[title_letters,0.5,true,true]]);
  };
 </script>

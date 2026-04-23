9f5f3500f813ff8698f544db253d8dc9d7098fd7
roryjamesallen <rory.james.allen1@gmail.com>
2026-04-23 16:27:53 +0100

    festival styling

--- festival/index.php
+++ festival/index.php
@@ -12,10 +12,11 @@ $event = getEventFromId($event_id);
 <!DOCTYPE html>
 <html lang="en">
     <head>
-	<base href="../">
+	<base href="https://seasoning.live">
 	<?php renderSEO('Seasoning Festival 2026 - Rave Culture Is Folk Culture', 'https://seasoning.live/festival'); ?>
 	<link rel="stylesheet" href="style.css?v=<?php echo file_get_contents($root.'css-version.txt'); ?>">
-	<link rel="stylesheet" href="festival/festival-style.css?v=4">
+	<link rel="stylesheet" href="festival/festival-style.css?v=5">
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
     </head>
     <?php echo $analytics ?>
 
@@ -24,9 +25,9 @@ $event = getEventFromId($event_id);
 	<img src="festival/seasoning-festival-logo.svg" id="festival-logo" alt="Logo for Seasoning Festival 2026">
 	<h1 style="display: none">Seasoning Festival 2026 - Rave Culture Is Folk Culture</h1>
 	<div style="height: 100vh"></div>
-	<img src="festival/blue-circle-background.svg" style="position: absolute; top: 100vh; left: 0; width: 100%">
-	<img src="festival/lightning-background.svg" style="position: absolute; top: 100vh; left: 0; width: 100%">
-	<img src="festival/lightning-background.svg" style="position: absolute; top: 300vh; left: 0; width: 100%">
+	<img src="festival/blue-circle-background.svg" style="position: absolute; top: 100vh; left: 0; width: 100%; z-index: -1" alt="Blue circles background image">
+	<img src="festival/lightning-background.svg" style="position: absolute; top: 100vh; left: 0; width: 100%; z-index: -1" alt="Yellow lightning background image">
+	<img src="festival/lightning-background.svg" style="position: absolute; top: 300vh; left: 0; width: 100%; z-index: -1" alt="Yellow lightning background image">
 	<div class="paragraph" style="margin-top: 3rem; gap: 2rem; position: relative">
 	    <h2 style="text-align: left; font-family: Milker; font-size: 3rem;"><span style="color: var(--title)"><span style="font-size: 4rem">Rave</span> Culture</span> <span style="color: var(--paragraph)"><br>Is <span style="font-size: 4rem">Folk</span> Culture</span></h2>
 	    <p style="text-align: right; font-size: 2rem; margin: 0">Not a weekend away from life, but a durable scene inside it. Something earthy, bass-led, porous and <strong>strange.</strong></p>
@@ -62,9 +63,14 @@ $event = getEventFromId($event_id);
 		};
 		echo '</div>';
 		?>
+		<p style="font-family: Milker; align-self: center; font-size: 2rem; margin: 0;">& Many More To Come</p>
 	    </div>
-	    <p style="font-family: Milker; align-self: center; font-size: 2rem; margin: 0">& Many More To Come</p>
-	    <div style="border: 2px solid var(--title); padding: 0.5rem; max-width: fit-content; margin: 0 auto; background-color: var(--background)">
+	    
+	    
+	    <p style="font-size: 2rem; margin: 0">A space where emerging artists stand alongside those shaping the wider landscape, and where new forms begin to take hold.</p>
+	    <h2 style="text-align: right; font-family: Milker; align-self: last baseline; margin-bottom: 0; color: var(--paragraph); font-size:2rem">A programme<br> built on <span style="font-size: 3rem; color: var(--title); display: inline-block; margin-bottom: -0.5rem">connection</span> not hierarchy</h2>
+	    
+	    <div style="border: 2px solid var(--title); padding: 0.5rem; max-width: fit-content; margin: 3rem auto 0; background-color: var(--background); font-size: 2rem">
 		<a href="https://fixr.co/event/seasoning-festival-2026-tickets-738401183?ref=MC3012" style="font-family: Milker;  color: var(--title);">Buy Tickets</a>
 	    </div>
 	    <hr>

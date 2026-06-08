0eb5c8e67406c1d648c923fa956c284ef0018646
roryjamesallen <rory@hogwild.uk>
2026-06-08 17:17:48 +0100

    headers order

--- index.php
+++ index.php
@@ -3,8 +3,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-include 'lib.php';
-
 session_start();
 //$_SESSION['popup'] = true;
 if (isset($_POST['email'])){
@@ -21,6 +19,8 @@ if (isset($_POST['email'])){
 } else {
     $popup = true;
 }
+
+include 'lib.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">

7fd30054f6c9a1a4a984197be88d0615fab14dd1
Rory Allen <rory@hogwild.uk>
2026-09-10 21:57:38 +0100

    redirect artist pages to instagram

--- artist/index.php
+++ artist/index.php
@@ -11,42 +11,21 @@ if (isset($_GET['a'])){
     $details = null;
     $spotlight_artists = json_decode(file_get_contents('../artists.json'), true);
     foreach ($spotlight_artists as $match_name => $potential_match){
-        if (isset($potential_match['permalink']) && $potential_match['permalink'] == $artist){
-            $artist = $match_name;
+        if (
+	    ((isset($potential_match['permalink']) && $potential_match['permalink'] == $artist)
+		or ($match_name == $artist))
+	    and isset($potential_match['instagram'])){
+	    header('Location: '.$potential_match['instagram']);
+	    exit();
+	    echo 'found '.$potential_match['instagram'];
+	    $artist = $match_name;
 	    $details = $potential_match;
         }
     }
+    header('Location: ../');
+    exit();
 } else {
     header('Location: ../');
+    exit();
 }
-
 ?>
-<!DOCTYPE html>
-<?php startup();?>
-<html lang="en">
-    <head>
-	<base href="../">
-	<?php renderSEO($artist.' - Seasoning Artist Spotlight', 'https://seasoning.live/artist/'.getArtistLink($artist, $details)); ?>
-	<link rel="stylesheet" href="style.css?v=<?php echo file_get_contents($root.'css-version.txt'); ?>">
-    </head>
-    <?php echo $analytics ?>
-    
-    <body>
-	<?php
-	renderTitle('<span style="opacity: 0.75">Artist Spotlight:</span> '.$artist);
-	renderArtistInfo($artist);
-	?>
-	
-	<div class="secondary-background">
-	    <?php
-            renderPageBreak(1, 'primary');
-	    renderUpcomingAndPastEvents($artist, ' featuring '.$artist);
-	    ?>
-	</div>
-	<?php renderPageBreak(2, 'secondary'); ?>
-    </body>
-
-    <?php renderFooter() ?>
-</html>
-
-<script type="module" src="scripts.js"></script>

a811817e2e4e64b178c2f8756fabfc43605452a8
roryjamesallen <rory@hogwild.uk>
2026-09-11 12:51:23 +0100

    save

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -2,9 +2,11 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd ) 
 ssh seasoning@seasoning.live '
 mv public_html/cgi-bin tmp/ &&
 mv public_html/php.ini tmp/ &&
+mv public_html/.htaccess tmp/ &&
 rm -rf public_html/* &&
 cp -r tmp/cgi-bin public_html/ &&
-cp tmp/php.ini public_html/' # Remove all public html contents apart from cgi-bin and php.ini
+cp tmp/php.ini public_html/ &&
+cp tmp/.htaccess public_html/' # Remove all public html contents apart from cgi-bin and php.ini and .htaccess
 scp -r $SCRIPT_DIR/../public_html/* seasoning@seasoning.live:public_html/ # Copy all to server public_html
 scp -r $SCRIPT_DIR/../lib.php seasoning@seasoning.live:public_html/ # Copy lib to server public_html
 ssh seasoning@seasoning.live 'mv public_html/lib.php ./' # Move lib to root
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd ) 
 #php $SCRIPT_DIR/generate_sitemap.php
 rm -rf $SCRIPT_DIR/../public_html/ # Delete any old built files
 mkdir $SCRIPT_DIR/../public_html # Make the build directory
-cp -rp $SCRIPT_DIR/../src/* $SCRIPT_DIR/../public_html/ # Copy every file and dir from src to public_html
+cp -rp $SCRIPT_DIR/../src/. $SCRIPT_DIR/../public_html/ # Copy every file and dir from src to public_html

950eb9bb9f7a83c9caeaf1d3c982e275454b94d6
roryjamesallen <rory@hogwild.uk>
2026-09-11 12:51:50 +0100

    add preview faq

---
+++ src/faq/index.php
@@ -0,0 +1,33 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$root = '../';
+include '../../lib.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+	<base href="../">
+	<?php
+	renderSEO('Frequently Asked Questions', 'https://seasoning.live/faq', 'Find an answer to all of your most pressing Seasoning questions.');
+	?>
+	<link rel="stylesheet" href="style.css?v=<?php echo file_get_contents($root.'css-version.txt'); ?>">
+    </head>
+    <?php echo $analytics ?>
+    <body>
+	<?php renderTitle('Frequently Asked Questions'); ?>
+        <div class="faq-container paragraph">
+	    <h3>Question?</h3>
+	    <p>Please email bookings@seasoning.live for enquiries relating to booking an artist.</p>
+	    <h3>Question?</h3>
+	    <p>Or email production@seasoning.live for anything relating to other Seasoning goings on!</p>
+	</div>
+    </body>
+
+    <?php renderFooter(); ?>
+</html>
+
+<script type="module" src="scripts.js"></script>

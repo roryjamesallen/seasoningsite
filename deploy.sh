b2c384084aafa7c360a1d2bc27a44033060696b6
roryjamesallen <rory@hogwild.uk>
2026-09-11 10:30:27 +0100

    fixes

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd ) # Get the path of this script
-rm -rf $SCRIPT_DIR/../public_html/ # Delete any old built files
-mkdir $SCRIPT_DIR/../public_html # Make the build directory
-cp -rp $SCRIPT_DIR/../src/* $SCRIPT_DIR/../public_html/ # Copy every file and dir from src to public_html
--- scripts/copy.sh
+++ scripts/copy.sh
@@ -1,2 +1,10 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd ) # Get the path of this script
-scp -P 9284 -rp $SCRIPT_DIR/../public_html/ seasoning@seasoning.live:development/ # Copy all to server
+ssh seasoning@seasoning.live '
+mv public_html/cgi-bin tmp/ &&
+mv public_html/php.ini tmp/ &&
+rm -rf public_html/* &&
+cp -r tmp/cgi-bin public_html/ &&
+cp tmp/php.ini public_html/' # Remove all public html contents apart from cgi-bin and php.ini
+scp -r $SCRIPT_DIR/../public_html/* seasoning@seasoning.live:public_html/ # Copy all to server public_html
+scp -r $SCRIPT_DIR/../lib.php seasoning@seasoning.live:public_html/ # Copy lib to server public_html
+ssh seasoning@seasoning.live 'mv public_html/lib.php ./' # Move lib to root

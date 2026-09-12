d007ed68d7a713ee7c3ecfc0073aaaed9b199bf5
Rory Allen <rory@hogwild.uk>
2026-09-12 10:53:51 +0100

    fix gallery

--- src/gallery/index.php
+++ src/gallery/index.php
@@ -15,30 +15,23 @@ include '../../lib.php';
     <body>							
 	<?php renderTitle('Gallery'); ?>
 	
-	<div class="paragraph" id="gallery">
-	    <div class="gallery-container">
+	    <div class="gallery-container paragraph">
 		<h3>Goods Yard</h3>
 		<div class="gallery-gradient"></div>
 		<div class="gallery">
-		    <?php renderPhoto('seasoning-festival-photo-1.jpg', 'Fabric sails at Seasoning Festival 2026, Stroud', 'Samuel Wilson') ?>
-		    <img src="images/gallery/seasoning-festival-photo-2.jpg" loading="lazy" alt="Attendees outside The SVA, Stroud at Seasoning Festival 2026" draggable="false">
-		    <img src="images/gallery/seasoning-festival-photo-4.jpg" loading="lazy" alt="Person wearing Seasoning Festival 2026 T-Shirt" draggable="false">
-		    <img src="images/gallery/seasoning-festival-photo-5.jpg" loading="lazy" alt="Pizzas being served at Seasoning Festival 2026, Stroud" draggable="false">
-		    <img src="images/gallery/seasoning-festival-photo-6.jpg" loading="lazy" alt="Wall art being drawn at Seasoning Festival 2026, Stroud" draggable="false">
-		    <img src="images/gallery/seasoning-festival-photo-7.jpg" loading="lazy" alt="Printing press beig used at Seasoning Festival 2026, Stroud" draggable="false">
- 		    <img src="images/gallery/seasoning-festival-photo-8.jpg" loading="lazy" alt="Hand painted blue and pink moon for Seasoning Festival 2026, Stroud" draggable="false">
-		    <img src="images/gallery/seasoning-festival-photo-9.jpg" loading="lazy" alt="Hay bales and fabric flags at Seasoning Festival 2026, Stroud" draggable="false">
-		    <img src="images/gallery/seasoning-festival-photo-10.jpg" loading="lazy" alt="A person in the stocks at Seasoning Festival 2026, Stroud" draggable="false">
-		    <img src="images/gallery/seasoning-festival-photo-11.jpg" loading="lazy" alt="The Goods Yard outside bar at Seasoning Festival 2026, Stroud" draggable="false">
-		    <img src="images/gallery/seasoning-festival-photo-3.jpg" loading="lazy" alt="Attendees at at Seasoning Festival 2026, Stroud" draggable="false">
+		    <?php
+		    renderPhoto('seasoning-festival-photo-1.jpg', 'Fabric sails at Seasoning Festival 2026, Stroud', 'Samuel Wilson');
+		    renderPhoto('seasoning-festival-photo-2.jpg', 'Attendees outside The SVA, Stroud at Seasoning Festival 2026', 'Samuel Wilson');
+		    renderPhoto('seasoning-festival-photo-4.jpg', 'Attendees outside The SVA, Stroud at Seasoning Festival 2026', 'Samuel Wilson');
+		    ?>
 		</div>
 	    </div>
-	</div>	
-
-	<div class="secondary-background">
+	    <br>
+	    <div class="secondary-background">
 	    <?php
 	    renderPageBreak(1, 'primary');
 	    ?>
+		<br>
 	    <div class="paragraph" id="gallery">
 		<div class="gallery-container">
 		    <h3 style="color: var(--beige)">Goods Shed</h3>

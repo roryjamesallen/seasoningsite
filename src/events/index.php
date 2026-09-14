9186debcf6dc1dc863a9b6f4a9f169b0d7306365
roryjamesallen <rory@hogwild.uk>
2026-09-14 10:36:21 +0100

    prevent hover z error

--- src/events/index.php
+++ src/events/index.php
@@ -14,7 +14,7 @@ include '../../lib.php';
     <?php echo $analytics ?>
     <body>
 	<?php renderTitle(''); ?>
-	<div class="secondary-background" style="position: relative; z-index: -1">
+	<div class="secondary-background">
 	    <?php
 	     renderPageBreak(1, 'primary');
 	    renderUpcomingAndPastEvents(false,'. Watch this space...');
